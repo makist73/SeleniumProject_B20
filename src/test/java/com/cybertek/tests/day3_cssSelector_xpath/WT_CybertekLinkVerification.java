7a398e99795c031bc6b4eec2d80eabe66b7080f1
makist73 <makist73@gmail.com>
2020-09-07 23:39:47 -0400

    Selenium Class

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.day3_cssSelector_xpath;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WT_CybertekLinkVerification {
@@ -17,9 +18,17 @@ public class WT_CybertekLinkVerification {
         driver.get("http://practice.cybertekschool.com/forgot_password");
 
         // 3.Enter any email into input box
-        driver.findElement(By.name("email")).sendKeys("anyemail@domain.com");
+        //driver.findElement(By.name("email")).sendKeys("anyemail@domain.com");
+        WebElement emailInputBox = driver.findElement(By.name("email"));
+
+        emailInputBox.sendKeys("anyemail@domain.com");
+
+
         // 4.Click on Retrieve password
-        driver.findElement(By.id("form_submit")).click();
+       // driver.findElement(By.id("form_submit")).click();
+
+        WebElement retrivePasswordButton = driver.findElement(By.id("form_submit"));
+        retrivePasswordButton.click();
 
         // 5.Verify URL contains: Expected: “email_sent”
         String expectedInUrl="email_sent";
@@ -34,6 +43,30 @@ public class WT_CybertekLinkVerification {
         // 6.Verify textbox displayed the content as expected.
         // Expected: “Your e-mail’s been sent!”
 
+        // verify the text
+
+        WebElement conformationMessage= driver.findElement(By.name("confirmation_message"));
+
+        String actualText = conformationMessage.getText();// alt+Enter: shortcut to go&write the "actualText"
+        String expectedText = "Your e-mail's been sent!";
+
+        System.out.println("actualText = "+actualText);
+        System.out.println("expectedText= "+expectedText);
+        // ctrl+d for copy/paste one line
+
+        if (actualText.equals(expectedText)){
+            System.out.println("Header text value verification PASSED!");
+
+        }else{
+            System.out.println("Header text value verification FAILED!!!");
+        }
+        // verify if the web element is displayed or not
+
+        if (conformationMessage.isDisplayed()){
+            System.out.println("Conformation message is displayed. Verification PASSED!");
+        }else{
+            System.out.println("Conformation message is NOT displayed. Verification FAILED!!!");
+        }
 
 
 
