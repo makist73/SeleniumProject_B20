48d6b66508f26cf31bcf688138243bfd257d43bd
makist73 <makist73@gmail.com>
2020-09-18 20:49:41 -0400

    Tasks1

--- src/test/java/com/cybertek/tests/Task/CybertekVerifocation.java
+++ src/test/java/com/cybertek/tests/Task/CybertekVerifocation.java
@@ -1,4 +1,13 @@
 package com.cybertek.tests.Task;
 
 public class CybertekVerifocation {
+    public static void main(String[] args) {
+        //TC	#2:	Cybertek	verifications
+        //1. Open	Chrome	browser
+        //2. Go	to	https://practice.cybertekschool.com
+        //3. Verify	URL	contains
+        //Expected:	cybertekschool
+        //4. Verify	title:
+        // Expected:	Practice
+    }
 }
