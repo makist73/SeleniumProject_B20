428221b539c6d502f3436098bca80dc014c1fa08
makist73 <makist73@gmail.com>
2020-10-04 22:00:50 -0400

    checkbox practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes_radio/P3_Checkbox_Practice.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes_radio/P3_Checkbox_Practice.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day4_findElements_checkboxes_radio;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class P3_Checkbox_Practice {
 
@@ -11,7 +13,16 @@ public class P3_Checkbox_Practice {
         //1. Go to http://practice.cybertekschool.com/checkboxes
         WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.get("http://practice.cybertekschool.com/checkboxes");
+
+        // Locating the first checkbox
+        WebElement checkbox1= driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+
+        // Locating the second checkbox
+        WebElement checkbox2= driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+
         //2. Confirm checkbox #1 is NOT selected by default
+
+
         //3. Confirm checkbox #2 is SELECTED by default.
         //4. Click checkbox #1 to select it.
         //5. Click checkbox #2 to deselect it.
