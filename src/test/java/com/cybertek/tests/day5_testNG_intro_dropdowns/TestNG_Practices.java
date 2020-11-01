d68d64dd4c667d6c50746c25bf77901e3607d0ae
makist73 <makist73@gmail.com>
2020-11-01 12:14:19 -0500

    10/31/2020

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -1,9 +1,10 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
 import com.cybertek.utilities.WebDriverFactory;
-import org.omg.PortableInterceptor.ServerRequestInfo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -25,13 +26,16 @@ public class TestNG_Practices {
         // it will wait up to 15 seconds, if page is loaded before that it will continue.
         driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
 
+        // 1-go to google
+        driver.get("https://www.google.com");
+
     }
 
     @Test
     public void google_title_verification(){
 
         // 1-go to google
-        driver.get("https://www.google.com");
+        //driver.get("https://www.google.com"); // moved to beforeMethod to run for both tests
 
         // 2- Verify title is Google
         String expectedTitle="Google";
@@ -40,6 +44,20 @@ public class TestNG_Practices {
         Assert.assertEquals(actualTitle, expectedTitle, "Titles are not matching!");
 
     }
+    @Test
+    public void google_search_title_verification(){
+        // go to google --> this part will be taken care of in the beforeMethod
+
+        // search apple
+        WebElement searchBox= driver.findElement(By.name("q"));
+        searchBox.sendKeys("apple"+ Keys.ENTER);
+
+        // make sure title contains apple
+
+
+
+    }
+
     @AfterMethod
     public void tearDownMethod() throws InterruptedException {
         Thread.sleep(3000);
