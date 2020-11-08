acc348fe249641227e4b9fb969ba363692f099f7
makist73 <makist73@gmail.com>
2020-11-07 21:49:26 -0500

    11/07/2020

--- src/test/java/com/cybertek/utilities/SmartBearUtilities.java
+++ src/test/java/com/cybertek/utilities/SmartBearUtilities.java
@@ -13,9 +13,11 @@ public class SmartBearUtilities {
     //#2- Create a static method called loginToSmartBear
     //#3- This method simply logs in to SmartBear when you call it.
     //#4- Accepts WebDriver type as parameter
-    public static void loginToSmartBear(WebDriver driver){
-        //3. Enter username: “Tester”
 
+    public static void loginToSmartBear(WebDriver driver){
+
+
+        //3. Enter username: “Tester”
         WebElement usernameInput = driver.findElement(By.id("ctl00_MainContent_username"));
         usernameInput.sendKeys("Tester");
 
