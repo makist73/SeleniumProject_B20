974bd86dee49e40e0d796f8c86bb9cbb5dcff872
makist73 <makist73@gmail.com>
2020-11-26 16:39:10 -0500

    day9

--- src/test/java/com/cybertek/tests/day9_windows_tabs/WindowHandlingPractices.java
+++ src/test/java/com/cybertek/tests/day9_windows_tabs/WindowHandlingPractices.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day9_windows_tabs;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -20,7 +22,7 @@ public class WindowHandlingPractices {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://practice.cybertekschool.com/windows");
+
 
 
     }
@@ -37,7 +39,7 @@ public class WindowHandlingPractices {
 
         @Test
         public void p5_handling_windows(){
-
+        driver.get("http://practice.cybertekschool.com/windows");
         String actualTitle = driver.getTitle();
         System.out.println("actualTitle = " + actualTitle);
 
@@ -60,8 +62,51 @@ public class WindowHandlingPractices {
             for (String each : windowHandles) {   // Shortcut: type "iter" to get this
                 driver.switchTo().window(each);
                 System.out.println("Current title= "+ driver.getTitle());
+                System.out.println("Current Window Handle = " + driver.getWindowHandle());
             }
 
+            //Getting the current(actual) title, and comparing with expected title.
+            Assert.assertTrue(driver.getTitle().equals("New Window"));
+
+            BrowserUtils.wait(2);
+            driver.close(); // driver close will close the current focused window/tab
+
+            //If I want to switch back to main page, to continue with some other actions:
+            driver.switchTo().window(mainHandle);
+
+            //driver.quit will close all of the tabs that are opened in that session together
+            driver.quit();
+
+        }
+
+
+        @Test
+    public void p6_handling_more_than_two_windows(){
+
+            driver.get("https://amazon.com");
+            //THIS LINE IS BASICALLY : We are downcasting our driver type to JavaScriptExecutor
+            // THe only thing this line is doing is USING JS EXECUTOR -> opening new tabs with given links
+            ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+            ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+            ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
+            Set<String> windowHandles = driver.getWindowHandles();
+            //AMAZON
+            //FACEBOOK
+            //ETSY
+            //GOOGLE
+
+            for (String each : windowHandles){
+                BrowserUtils.wait(1);
+                driver.switchTo().window(each);
+                System.out.println("Current Page Title = " + driver.getTitle());
+
+                if (driver.getCurrentUrl().contains("etsy")){
+                    Assert.assertTrue(driver.getTitle().contains("Etsy"));
+                    break;
+                }
+
+            }
         }
 
 }
